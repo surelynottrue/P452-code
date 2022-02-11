6111f030f7ee7a3030f9b172211af1f10772d572
surelynottrue <surelynottrue@gmail.com>
2022-02-11 19:16:23 +0530

    correct junction formula, floating point issue persists

--- junction/josephson.jl
+++ junction/josephson.jl
@@ -18,37 +18,37 @@ function noise(I₁)
     μ = 0
     σ = 2 * I₁
     n = rand(Truncated(Normal(μ, σ), -3σ, 3σ), 1)[1]
-    return n
+    return 0
 end
 
 function junction(t, y, paramlist)
     ħ, e, Idc, C, R, I₁ = paramlist
     ϕ, dϕ = y
-    v = (ħ/(2*e)) * dϕ
-    ddϕ = Idc/C - (1/R)*(v/C) - (I₁/C) * sin(ϕ) + noise(I₁)/C
+    bigconst = (2*e)/(C*ħ)
+    ddϕ = (Idc - I₁*sin(ϕ))*bigconst - dϕ/(R*C) - noise(I₁)*bigconst
     return [dϕ, ddϕ]
 end
 
 αlist = 0.1:0.01:1.0
 
 tin = 0.0
-tfin = 1.5e-2
-h = 1e-4
+tfin = 5e-3
+h = 1e-5
 funcs = junction
 
 
 R = 1
-C = 2e-5
+C = 1e-5
 ħ = ustrip(ReducedPlanckConstant)
 e = ustrip(ElementaryCharge)
 Kb = ustrip(BoltzmannConstant)
-T = 100
+T = 10
 ϕ₀, dϕ₀ = varlist = [0.0 0.0;]
-# figure = plot()
+figure = plot()
 
-for γ in (1:1000:1000)
+for γ in (1:1e3:6e3)
     local vlist = []
-    local I₁ = γ*1e-9
+    local I₁ = γ * (e*Kb*T/ħ)
     println("Averaging time $tin to $tfin for γ = $γ:")
     for α in ProgressBar(αlist)
         local Idc = α * I₁
@@ -57,13 +57,13 @@ for γ in (1:1000:1000)
 
         local ϕ = varlist[:, 1]
         local dϕ = varlist[:, 2]
-        local avgnum = round(Int, 0.1 * length(varlist[:, 1]))
+        local avgnum = round(Int, 0.01 * length(varlist[:, 1]))
         local avgvec = last(dϕ, avgnum)
         local vavg = mean(skipmissing((ħ/(2*e)).*(dϕ)))
         push!(vlist, vavg)
     end
+    # println(R)
     local η = vlist ./ (I₁*R)
     plot!(η, αlist, label="γ = $γ")
     global varlist = [0.0 0.0;]
 end
-1
